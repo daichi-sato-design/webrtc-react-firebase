0696c3c41cfaf75658acfd2d2f64b9c1e9b37410
daichi-sato-design <daichisato.design@gmail.com>
2022-01-05 23:55:53 +0900

    clean up

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,7 @@ const App = () => {
         localPeerName={localPeerName}
         setLocalPeerName={setLocalPeerName}
       />
+
       <InputFormRemote
         localPeerName={localPeerName}
         remotePeerName={remotePeerName}
